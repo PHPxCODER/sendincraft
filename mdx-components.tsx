2707e93e1a5dbc2f3865ccb0b1cc47805301cbe5
Subhadeep <22051558@kiit.ac.in>
2025-07-13 15:08:01 +0530

    docs update

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,9 +1,19 @@
-import defaultMdxComponents from 'fumadocs-ui/mdx';
-import type { MDXComponents } from 'mdx/types';
+// mdx-components.tsx
 
-export function getMDXComponents(components?: MDXComponents): MDXComponents {
+import type { MDXComponents } from "mdx/types";
+import { Callout } from "fumadocs-ui/components/callout";
+import { Tab, Tabs } from "fumadocs-ui/components/tabs";
+import { TypeTable } from "fumadocs-ui/components/type-table"; // Good to have
+import defaultComponents from "fumadocs-ui/mdx"; // Imports all default components
+
+export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
-    ...defaultMdxComponents,
+    ...defaultComponents, // Use all the defaults from fumadocs-ui
     ...components,
+    // Add your custom components here
+    Callout,
+    Tabs,
+    Tab,
+    TypeTable,
   };
 }
